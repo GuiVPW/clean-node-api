5120fbc8402f67dc71027726b97366404a3f3add
Xubili <guivpw68@gmail.com>
2020-12-23 22:01:12 -0300

    test: ensure BcryptAdapter throws if bcrypt throws

--- src/infra/cryptography/bcrypt-adapter.spec.ts
+++ src/infra/cryptography/bcrypt-adapter.spec.ts
@@ -27,4 +27,14 @@ describe('BcryptAdapter', () => {
 
 		expect(hash).toBe('hash')
 	})
+
+	test('Should throw if bcrypt throws', async () => {
+		const sut = makeSut()
+		jest
+			.spyOn(bcrypt, 'hash')
+			.mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+		const promise = sut.encrypt({ value: 'any_value' })
+
+		await expect(promise).rejects.toThrow()
+	})
 })
