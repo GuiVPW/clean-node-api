858bb86cd3748cb8ebb67e6d9c0f3963841e3304
Xubili <guivpw68@gmail.com>
2020-12-24 11:03:16 -0300

    feat: ensure SignUp route returns an account on success

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,10 +1,12 @@
 import express from 'express'
 import setupMiddlewares from './middlewares'
+import setupRoutes from './routes'
 
 export const port = 3000 || process.env.PORT
 
 const app = express()
 
 setupMiddlewares(app)
+setupRoutes(app)
 
 export default app
