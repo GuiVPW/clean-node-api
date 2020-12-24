8b7587d13c5949b7779171d4b93bad5ef7998f1a
Xubili <guivpw68@gmail.com>
2020-12-24 12:13:26 -0300

    chore: configured jest coverage

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,10 @@
 export default {
 	roots: ['<rootDir>/src'],
-	collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+	collectCoverageFrom: [
+		'<rootDir>/src/**/*.ts',
+		'!<RootDir>/src/main/**',
+		'!<RootDir>/src/presentations/protocols/**'
+	],
 	coverageDirectory: 'coverage',
 	coverageProvider: 'v8',
 	testEnvironment: 'node',
